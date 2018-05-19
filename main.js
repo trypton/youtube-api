fcee51a2f8ec96a0d3d1fa16645c130060501297
Karen Ananiev <karen.ananiev@gmail.com>
2018-05-19 16:07:45 +0300

    move files to src folder

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-import { YouTubeSearch } from './YouTubeSearch';
-export { YouTubeSearch };
+import { YouTubeSearch } from './src/YouTubeSearch.js';
+import { YouTubeVideos } from './src/YouTubeVideos.js';
+export { YouTubeSearch, YouTubeVideos };
